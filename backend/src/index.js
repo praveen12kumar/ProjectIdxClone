9615a569a6166ddb366fbcf08edefb5258ba0b36
Praveen Kumar <praveenshakya441@gmail.com>
2024-12-13 16:16:42 +0530

    Added basic sockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,29 @@
 import express from 'express';
-
+import {createServer} from 'node:http';
 import {PORT} from "./config/serverConfig.js";
 import cors from 'cors';
 import apiRouter from "./routes/index.js";
+import {Server} from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin:'*',
+        methods:['GET', 'POST']
+    }
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+})
+
 
 app.use('/api', apiRouter);
 
@@ -16,6 +31,6 @@ app.get('/ping', (req, res)=>{
    return res.json({message: 'pong'})
 })
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`server is running on PORT: ${PORT}`);
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import Router from './Router'
 import './App.css'
+import {io} from 'socket.io-client';
 
 function App() {
   
+  const socket = io("http://localhost:8000");
 
   return (
     <>  
